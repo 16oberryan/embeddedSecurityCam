72c1b8dd760d5b133ca2e06cb88dd87706086643
Oluwaseun Ladipo <ladipooo@rose-hulman.edu>
2019-11-19 06:00:37 -0500

    typos

--- ManualControl.js
+++ ManualControl.js
@@ -16,7 +16,6 @@ var blynk = new Blynk.Blynk(AUTH);
 var v4 = new blynk.VirtualPin(4);
 var v5 = new blynk.VirtualPin(5);
 
-//b.analogWrite(VSERV, (90*.064)/100, 60);
 
 function printStatus(x){
         console.log("woo")
@@ -29,8 +28,8 @@ v4.on('write', function(param) {
 });
 
 v5.on('write', function(param) {
-      var duty_cycle = param[0]/100;
+      var duty_cycle = param[0]*.064;
       console.log('Horizontal:', duty_cycle);
-      b.analogWrite(HSERV, duty_cycle);
+      b.analogWrite(HSERV, duty_cycle/100, 60);
 });
 
